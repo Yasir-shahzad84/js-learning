23bcb4f79befb446ca3ec0448f97fc58adc8ec1b
Yasir-shahzad84 <yasir_lbin@hotmail.com>
2025-03-24 13:15:10 +0000

    variables class 1

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 122334 
+let accountEmail = "ckeybord@gmail.com"
+let accountPassword = "122334"
+let accountCity = "Lahore"
+let accountState;
+
+// accountId = 2 //note allowed
+accountEmail = "hc@hc.com"
+accountPassword = "11223344"
+accountCity ="islamabad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState] );
