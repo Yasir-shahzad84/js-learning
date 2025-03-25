c27fba18da518faa94db089e14f16480202defaa
Yasir-shahzad84 <yasir_lbin@hotmail.com>
2025-03-25 10:34:24 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Yasir"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// "Yasir" => NaN
+
+let isLoggedIn = "yasir"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => ture
+// 0 -> false
+// "" => false
+// "yasir" => true
+
+let someNumber = 88
+
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
+
