a844bb0e0130787025e214d1791492fa4ca1891a
Yasir-shahzad84 <yasir_lbin@hotmail.com>
2025-03-25 10:01:45 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat akk JS code as newer version 
+
+// alert (3 + 3) // we are using node.js, not a browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
